3a99dea2339bfd8b132ff37b94247ea035197212
Ilya Kulakov <kulakov.ilya@gmail.com>
2011-12-12 22:15:26 +0700

    Add MySQL_python requirenment.

--- setup.py
+++ setup.py
@@ -15,6 +15,7 @@ setup(
     install_requires=[
         "Pylons>=1.0",
         "SQLAlchemy>=0.5",
+        "MySQL_python>=1.2.3"
     ],
     setup_requires=["PasteScript>=1.6.3"],
     packages=find_packages(exclude=['ez_setup']),
