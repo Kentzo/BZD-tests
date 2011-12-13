44da4a7f4294b78062fefdf4f5f7d37243c3886a
Ilya Kulakov <kulakov.ilya@gmail.com>
2011-12-13 20:58:39 +0700

    don't recreate tables at start.

--- bzdtests/model/__init__.py
+++ bzdtests/model/__init__.py
@@ -10,7 +10,6 @@ from sqlalchemy import schema, types
 def init_model(engine):
     """Call me before using any of the tables or classes in the model"""
     Session.configure(bind=engine)
-    Base.metadata.create_all(bind=Session.bind)
 
 
 class TestSuite(Base):
